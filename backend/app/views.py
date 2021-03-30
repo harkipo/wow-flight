d4eb0c3de349969e272916644addb92b13487987
harshit <hrastogi43@gmail.com>
2021-03-30 20:48:07 +0530

    backend view update

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,9 +1,7 @@
-from rest_framework import status, mixins
 from rest_framework.response import Response
 from .serializer import FlightSerializer
 from .models import *
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, GenericViewSet
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 
 
